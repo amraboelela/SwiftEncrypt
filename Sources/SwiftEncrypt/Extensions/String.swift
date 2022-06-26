2aec0d14d9067f98bf31962bbbf6f9dd94a05084
Amr Aboelela <amraboelela@gmail.com>
2022-06-26 12:21:45 -0700

    public static

--- Sources/SwiftEncrypt/Extensions/String.swift
+++ Sources/SwiftEncrypt/Extensions/String.swift
@@ -11,10 +11,10 @@ import CommonCrypto
 
 extension String {
     
-    static let base32Codes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",
+    public static let base32Codes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",
     "G", "H", "J", "K", "M", "N", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    static let base32ForbiddenLetters: Set<String> = ["I", "L", "O", "P"]
-    static let base32DecodeMap = ["0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15, "G": 16, "H": 17, "J": 18, "K": 19, "M": 20, "N": 21, "Q": 22, "R": 23, "S": 24, "T": 25, "U": 26, "V": 27, "W": 28, "X": 29, "Y": 30, "Z": 31]
+    public static let base32ForbiddenLetters: Set<String> = ["I", "L", "O", "P"]
+    public static let base32DecodeMap = ["0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15, "G": 16, "H": 17, "J": 18, "K": 19, "M": 20, "N": 21, "Q": 22, "R": 23, "S": 24, "T": 25, "U": 26, "V": 27, "W": 28, "X": 29, "Y": 30, "Z": 31]
     
     func MD5(string: String) -> Data {
         let length = Int(CC_MD5_DIGEST_LENGTH)
